dff4dc96b39561aa4eb0a6f89700662ba3025b7a
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-03-03 01:40:06 +0700

    Table standar remove unused code for example

--- resources/views/modules/example/grids/example_grid.blade.php
+++ resources/views/modules/example/grids/example_grid.blade.php
@@ -43,19 +43,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @for($i=1; $i<=20; $i++)
-                                    <tr>
-                                        <td>{{ $i }}</td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                @endfor
+
                                 </tbody>
                                 <tfoot></tfoot>
                             </table>
