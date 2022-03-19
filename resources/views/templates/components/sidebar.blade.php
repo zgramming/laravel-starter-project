0648b35ee7cb37f02ee9335490ea664200caf0bf
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-03-19 13:21:26 +0700

    Fix wrong path logo mazer

--- resources/views/templates/components/sidebar.blade.php
+++ resources/views/templates/components/sidebar.blade.php
@@ -15,7 +15,7 @@ $currentSegment = implode("/",request()->segments());
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                    <a href="index.html"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo" srcset=""></a>
                 </div>
                 <div class="toggler">
                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
