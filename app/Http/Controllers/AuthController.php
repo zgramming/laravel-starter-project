be64968439c1459e6bdb441b56de80bc0f19aa63
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-04-02 21:39:21 +0700

    Adding information using username instead of email for authentication in AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,6 +30,9 @@ class AuthController extends Controller
      */
     public function login(): Redirector|RedirectResponse|Application
     {
+        /// If you want change authentication method using username instead of email, you can follow link in below.
+        /// Don't forget to change input type from email to username in login.blade.php >.<
+        /// Reference [https://stackoverflow.com/a/31852437/7360353]
 
         try {
             $post = request()->all();
