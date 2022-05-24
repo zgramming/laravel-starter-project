52a14cd76fcbd135dc33b67095fbe1ade0e885dc
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-05-24 13:04:48 +0700

    full width success message

--- resources/views/templates/components/messages/success/withsuccess.blade.php
+++ resources/views/templates/components/messages/success/withsuccess.blade.php
@@ -1,5 +1,5 @@
 @if($message)
-    <div class="alert alert-success alert-dismissible show fade">
+    <div class="alert alert-success alert-dismissible show fade w-100">
         {{$message}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
