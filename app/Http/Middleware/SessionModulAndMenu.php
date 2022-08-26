d07364ae8077863eaf78964eaa06266704ac0e76
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-08-26 14:06:53 +0700

    Fix nullable menu session

--- app/Http/Middleware/SessionModulAndMenu.php
+++ app/Http/Middleware/SessionModulAndMenu.php
@@ -23,18 +23,18 @@ class SessionModulAndMenu
         /// Save Session Modul And Menu
         $currentSegments = $request->segments();
         list($firstSegment, $secondSegment) = $currentSegments;
-        $menu = Menu::where("route", "like", "%".sprintf("%s/%s", $firstSegment, $secondSegment))->get();
+        $menu = Menu::where("route", "like", "%" . sprintf("%s/%s", $firstSegment, $secondSegment))->get();
 
         /// If true, we assume this `menu` has parent
         /// Then we should get third segment to filter it
         if ($menu->count() > 1) {
             list($firstSegment, $secondSegment, $thirdSegment) = $currentSegments;
-            $menu = $menu->filter(fn(Menu $value, $key) => $value->route === sprintf("%s/%s/%s", $firstSegment, $secondSegment, $thirdSegment))->first();
+            $menu = $menu->filter(fn (Menu $value, $key) => $value->route === sprintf("%s/%s/%s", $firstSegment, $secondSegment, $thirdSegment))->first();
         } else {
             $menu = $menu->first();
         }
 
-        $dataSession = ['menu_id' => $menu->id, 'modul_id' => $menu->app_modul_id];
+        $dataSession = ['menu_id' => $menu?->id, 'modul_id' => $menu?->app_modul_id];
 
         session([Constant::KEY_SESSION_MODUL_MENU => $dataSession]);
 
